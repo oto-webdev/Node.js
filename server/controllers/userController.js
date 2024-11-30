cded7f3df1baaf3e8127b935f93b9ee5d4251e71
oto-webdev <r.otowebdev@gmail.com>
2024-11-30 22:42:58 +0400

    auth

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,64 @@
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
+import expressAsyncHandler from 'express-async-handler'
 import User from '../models/User.js'
+
+export const register = expressAsyncHandler(async (req, res) => {
+    const {username, email, password, role} = req.body;
+    if(!username || !email || !password || !role) {
+        return res.status(401).json({message: "All fields are required"})
+    }
+
+    const salt = await bcrypt.genSalt()
+    const hashed = await bcrypt.hash(password, salt)
+
+    const exists = await User.findOne({email})
+    if(exists) {
+        return res.status(409).json({message: "Email exists"})
+    }
+
+    try{
+        const user = await User.create({
+            username,
+            email,
+            password: hashed,
+            role: role || 'user'
+        })
+        await user.save()
+        return res.status(201).json({message: "New User", user})
+    }catch(error){
+        return res.status(500).json({message: error.message})
+    }
+})
+
+export const login = expressAsyncHandler(async (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const user = await User.findOne({ username });
+    if (!user) {
+        return res.status(404).json({ message: "Invalid username" });
+    }
+
+    const matched = await bcrypt.compare(password, user.password);
+    if (!matched) {
+        return res.status(401).json({ message: "Password does not match" });
+    }
+
+    try{
+        return res.status(200).json({ 
+            message: "Login successful", 
+            user: { 
+                id: user._id, 
+                username: user.username, 
+                email: user.email, 
+                role: user.role 
+            } 
+        });
+    }catch(error){
+        return res.status(500).json({message: error.message})
+    }
+});
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express'
+import { login, register } from '../controllers/userController.js';
 
 const router = express.Router()
 
-router.post("/", )
-router.post("/login", )
+router.post("/", register)
+router.post("/login", login)
 
 export default router;
