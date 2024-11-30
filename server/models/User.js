10e8f4b0f2df3c57e4b746fe456ecb804d327955
oto-webdev <r.otowebdev@gmail.com>
2024-11-30 21:45:59 +0400

    user model

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,38 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
 
-    
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate: {
+            validator: (value) => {
+                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value); 
+            },
+            message: "Invalid email format."
+        }
+    },    
+
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user'
+    }
 
 }, {
     timestamps: true
